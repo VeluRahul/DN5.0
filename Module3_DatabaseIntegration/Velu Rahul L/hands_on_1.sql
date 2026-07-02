45106d437a64512870d0a8649324acccea46c382
Rahul <velurediff@gmail.com>
2026-07-02 10:54:59 +0530

    Completed hands_on_1.sql

--- Module3_DatabaseIntegration/Velu Rahul L/hands_on_1.sql
+++ Module3_DatabaseIntegration/Velu Rahul L/hands_on_1.sql
@@ -1,4 +1,4 @@
--- TASK 1 : CREATE DATABASE
+-- TASK 1 : Create database
 
 CREATE DATABASE college_db;
 
@@ -6,97 +6,25 @@ USE college_db;
 
 -- CREATE TABLE : departments 
 
-CREATE TABLE departments
-(
-    department_id INT AUTO_INCREMENT PRIMARY KEY,
-
-    dept_name VARCHAR(100) NOT NULL,
-
-    hod_name VARCHAR(100),
-
-    budget DECIMAL(12,2)
-);
+CREATE TABLE departments(department_id INT AUTO_INCREMENT PRIMARY KEY,dept_name VARCHAR(100) NOT NULL,hod_name VARCHAR(100),budget DECIMAL(12,2));
 
 -- CREATE TABLE : students
 
-CREATE TABLE students
-(
-    student_id INT AUTO_INCREMENT PRIMARY KEY,
-
-    first_name VARCHAR(50) NOT NULL,
-
-    last_name VARCHAR(50) NOT NULL,
-
-    email VARCHAR(100) UNIQUE NOT NULL,
-
-    date_of_birth DATE,
-
-    department_id INT,
-
-    enrollment_year INT,
-
-    FOREIGN KEY (department_id)
-    REFERENCES departments(department_id)
-);
+CREATE TABLE students(student_id INT AUTO_INCREMENT PRIMARY KEY,first_name VARCHAR(50) NOT NULL,last_name VARCHAR(50) NOT NULL,email VARCHAR(100) UNIQUE NOT NULL,date_of_birth DATE,department_id INT,enrollment_year INT,FOREIGN KEY (department_id) REFERENCES departments(department_id));
 
 -- CREATE TABLE : courses
 
-CREATE TABLE courses
-(
-    course_id INT AUTO_INCREMENT PRIMARY KEY,
-
-    course_name VARCHAR(150) NOT NULL,
-
-    course_code VARCHAR(20) UNIQUE,
-
-    credits INT,
-
-    department_id INT,
-
-    FOREIGN KEY (department_id)
-    REFERENCES departments(department_id)
-);
+CREATE TABLE courses(course_id INT AUTO_INCREMENT PRIMARY KEY,course_name VARCHAR(150) NOT NULL,course_code VARCHAR(20) UNIQUE,credits INT,department_id INT,FOREIGN KEY (department_id)REFERENCES departments(department_id));
 
 -- CREATE TABLE : enrollments
 
-CREATE TABLE enrollments
-(
-    enrollment_id INT AUTO_INCREMENT PRIMARY KEY,
-
-    student_id INT,
-
-    course_id INT,
-
-    enrollment_date DATE,
-
-    grade CHAR(2),
-
-    FOREIGN KEY(student_id)
-    REFERENCES students(student_id),
-
-    FOREIGN KEY(course_id)
-    REFERENCES courses(course_id)
-);
+CREATE TABLE enrollment(enrollment_id INT AUTO_INCREMENT PRIMARY KEY,student_id INT,course_id INT,enrollment_date DATE,grade CHAR(2),FOREIGN KEY(student_id)REFERENCES students(student_id),FOREIGN KEY(course_id)REFERENCES courses(course_id));
 
 -- CREATE TABLE : professors
 
-CREATE TABLE professors
-(
-    professor_id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE professors(professor_id INT AUTO_INCREMENT PRIMARY KEY,prof_name VARCHAR(100) NOT NULL,email VARCHAR(100) UNIQUE,department_id INT,salary DECIMAL(10,2),FOREIGN KEY(department_id)REFERENCES departments(department_id));
 
-    prof_name VARCHAR(100) NOT NULL,
-
-    email VARCHAR(100) UNIQUE,
-
-    department_id INT,
-
-    salary DECIMAL(10,2),
-
-    FOREIGN KEY(department_id)
-    REFERENCES departments(department_id)
-);
-
--- VERIFY TABLE STRUCTURE
+-- verify table structure
 
 DESCRIBE departments;
 
@@ -108,7 +36,7 @@ DESCRIBE enrollments;
 
 DESCRIBE professors;
 
--- TASK 2 : NORMALIZATION ANALYSIS
+-- TASK 2 : normalisation analysis
 
 -- 1NF
 -- Every column stores only one value.
@@ -129,47 +57,29 @@ DESCRIBE professors;
 -- This avoids redundancy and maintains Third Normal Form.
 -- Enrollment contains only enrollment-specific data.
 
--- TASK 3 : ALTER TABLE
+-- TASK 3 : Alter Table
 
 -- Added phone number by using the following query
 
-ALTER TABLE students
-
-ADD phone_number VARCHAR(15);
+ALTER TABLE students ADD phone_number VARCHAR(15);
 
 -- Added maximum seats by using the following query
 
-ALTER TABLE courses
-
-ADD max_seats INT DEFAULT 60;
+ALTER TABLE courses ADD max_seats INT DEFAULT 60;
 
 -- Add CHECK constraint by using the following query
 
-ALTER TABLE enrollments
-
-ADD CONSTRAINT chk_grade
-
-CHECK
-(
-grade IN
-('A','B','C','D','F')
-
-OR grade IS NULL
-);
+ALTER TABLE enrollments ADD CONSTRAINT chk_grade CHECK(grade IN ('A','B','C','D','F') OR grade IS NULL);
 
 -- Rename column
 
-ALTER TABLE departments
-
-RENAME COLUMN hod_name TO head_of_dept;
+ALTER TABLE departments RENAME COLUMN hod_name TO head_of_dept;
 
 -- Drop phone number
 
-ALTER TABLE students
+ALTER TABLE students DROP COLUMN phone_number;
 
-DROP COLUMN phone_number;
-
--- VERIFY FINAL STRUCTURE
+-- to verify final structure
 
 DESCRIBE departments;
 
