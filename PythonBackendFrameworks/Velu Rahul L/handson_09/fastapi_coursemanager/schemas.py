f50f73edbc14c29c9968ecb51ecf1e89be631fcc
Rahul <velurediff@gmail.com>
2026-07-28 13:02:17 +0530

    Create schemas.py

---
+++ PythonBackendFrameworks/Velu Rahul L/handson_09/fastapi_coursemanager/schemas.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel
+from pydantic import EmailStr
+
+
+class UserRegister(BaseModel):
+
+    email: EmailStr
+
+    password: str
+
+
+class UserLogin(BaseModel):
+
+    email: EmailStr
+
+    password: str
+
+
+class Token(BaseModel):
+
+    access_token: str
+
+    token_type: str
+
+
+class UserResponse(BaseModel):
+
+    id: int
+
+    email: EmailStr
+
+    is_active: bool
+
+    class Config:
+
+        from_attributes = True
