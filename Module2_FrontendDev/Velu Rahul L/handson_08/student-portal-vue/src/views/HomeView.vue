95f004ec278d14bc3120bf65d247d139e2e54f47
Rahul <velurediff@gmail.com>
2026-07-28 11:09:55 +0530

    Add HomeView component for student portal

---
+++ Module2_FrontendDev/Velu Rahul L/handson_08/student-portal-vue/src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+
+<div class="container">
+
+<h1>
+
+Welcome to Student Portal
+
+</h1>
+
+<p>
+
+Vue.js 3 Application
+
+</p>
+
+</div>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+.container{
+
+padding:30px;
+
+}
+
+</style>
