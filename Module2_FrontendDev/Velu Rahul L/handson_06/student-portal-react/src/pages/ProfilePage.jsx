494092e3ab607af39309205ba61600f8f1e2eabe
Rahul <velurediff@gmail.com>
2026-07-28 10:45:42 +0530

    Refactor ProfilePage component for better readability

--- Module2_FrontendDev/Velu Rahul L/handson_06/student-portal-react/src/pages/ProfilePage.jsx
+++ Module2_FrontendDev/Velu Rahul L/handson_06/student-portal-react/src/pages/ProfilePage.jsx
@@ -1,4 +1,32 @@
+import { useContext }
+
+from "react";
+
+import {
+
+EnrollmentContext
+
+}
+
+from "../context/EnrollmentContext";
+
 function ProfilePage(){
+
+const{
+
+enrolledCourses,
+
+remove
+
+}
+
+=
+
+useContext(
+
+EnrollmentContext
+
+);
 
 return(
 
@@ -6,27 +34,85 @@ return(
 
 <h1>
 
-Student Profile
+Profile
 
 </h1>
 
+<h2>
+
+Enrolled Courses
+
+</h2>
+
+{
+
+enrolledCourses.length===0 ?
+
+(
+
 <p>
 
-Name : Rahul
+No Courses Enrolled
 
 </p>
 
+)
+
+:
+
+(
+
+enrolledCourses.map(course=>(
+
+<div
+
+key={course.id}
+
+style={{
+
+border:"1px solid gray",
+
+padding:"15px",
+
+marginBottom:"15px"
+
+}}
+
+>
+
+<h3>
+
+{course.name}
+
+</h3>
+
 <p>
 
-Department : Computer Science
+{course.code}
 
 </p>
 
-<p>
+<button
 
-Semester : 6
+onClick={()=>
 
-</p>
+remove(course.id)
+
+}
+
+>
+
+Remove
+
+</button>
+
+</div>
+
+))
+
+)
+
+}
 
 </div>
 
