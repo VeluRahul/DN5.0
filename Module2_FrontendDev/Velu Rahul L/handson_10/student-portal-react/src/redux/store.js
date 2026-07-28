2ed534cd800e0f28b805eeaa401facbd6f73766a
Rahul <velurediff@gmail.com>
2026-07-28 11:28:55 +0530

    Add Redux store configuration for courses

---
+++ Module2_FrontendDev/Velu Rahul L/handson_10/student-portal-react/src/redux/store.js
@@ -0,0 +1,21 @@
+import {
+
+configureStore
+
+}
+
+from "@reduxjs/toolkit";
+
+import courseReducer
+
+from "./courseSlice";
+
+export const store = configureStore({
+
+reducer:{
+
+courses:courseReducer
+
+}
+
+});
