7115c1ba1d7309dc578f098c5aac1c2c6474f3c7
Rahul <velurediff@gmail.com>
2026-07-28 10:19:05 +0530

    Implement course enrollment and search features

--- Module2_FrontendDev/Velu Rahul L/handson_05/student-portal-react/src/App.jsx
+++ Module2_FrontendDev/Velu Rahul L/handson_05/student-portal-react/src/App.jsx
@@ -1,1 +1,129 @@
+import { useState } from "react";
 
+import "./App.css";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import CourseCard from "./components/CourseCard";
+
+import { coursesData } from "./data";
+
+function App(){
+
+const [courses] = useState(coursesData);
+
+const [searchTerm,setSearchTerm]=useState("");
+
+const [enrolledCourses,setEnrolledCourses]=useState([]);
+
+function handleEnroll(course){
+
+const exists=enrolledCourses.find(
+
+item=>item.id===course.id
+
+);
+
+if(!exists){
+
+setEnrolledCourses(
+
+[...enrolledCourses,course]
+
+);
+
+}
+
+}
+
+const filteredCourses=courses.filter(course=>
+
+course.name.toLowerCase().includes(
+
+searchTerm.toLowerCase()
+
+)
+
+);
+
+return(
+
+<div className="app">
+
+<Header
+
+siteName="Student Portal"
+
+count={enrolledCourses.length}
+
+/>
+
+<h1
+style={{
+marginTop:"25px"
+}}
+>
+
+Available Courses
+
+</h1>
+
+<input
+
+type="text"
+
+placeholder="Search Courses..."
+
+className="search-box"
+
+value={searchTerm}
+
+onChange={(e)=>
+
+setSearchTerm(e.target.value)
+
+}
+
+/>
+
+<div className="course-container">
+
+{
+
+filteredCourses.map(course=>(
+
+<CourseCard
+
+key={course.id}
+
+name={course.name}
+
+code={course.code}
+
+credits={course.credits}
+
+grade={course.grade}
+
+onEnroll={()=>
+
+handleEnroll(course)
+
+}
+
+/>
+
+))
+
+}
+
+</div>
+
+<Footer/>
+
+</div>
+
+);
+
+}
+
+export default App;
