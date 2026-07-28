23e56bddf421c23389b6d94ff9f089f845b9a1a8
Rahul <velurediff@gmail.com>
2026-07-28 11:08:34 +0530

    Add App.vue with template, script, and styles

---
+++ Module2_FrontendDev/Velu Rahul L/handson_08/student-portal-vue/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+
+<Header />
+
+<RouterView />
+
+</template>
+
+<script setup>
+
+import Header from "./components/Header.vue";
+
+import { RouterView } from "vue-router";
+
+</script>
+
+<style>
+
+body{
+
+margin:0;
+
+font-family:Arial, Helvetica, sans-serif;
+
+background:#f5f5f5;
+
+}
+
+</style>
