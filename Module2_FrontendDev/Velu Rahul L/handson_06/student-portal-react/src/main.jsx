555b5a13fa0544accb962fb80baedeb54ae669be
Rahul <velurediff@gmail.com>
2026-07-28 10:47:03 +0530

    Integrate Redux Provider into main component
    Wrap the application in Redux Provider and remove EnrollmentProvider.

--- Module2_FrontendDev/Velu Rahul L/handson_06/student-portal-react/src/main.jsx
+++ Module2_FrontendDev/Velu Rahul L/handson_06/student-portal-react/src/main.jsx
@@ -3,24 +3,24 @@ import ReactDOM from "react-dom/client";
 
 import { BrowserRouter } from "react-router-dom";
 
+import { Provider } from "react-redux";
+
+import { store } from "./redux/store";
+
 import App from "./App";
 
 import "./index.css";
 
-import { EnrollmentProvider }
-
-from "./context/EnrollmentContext";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
 
-<BrowserRouter>
+    <Provider store={store}>
 
-<EnrollmentProvider>
+        <BrowserRouter>
 
-<App/>
+            <App />
 
-</EnrollmentProvider>
+        </BrowserRouter>
 
-</BrowserRouter>
+    </Provider>
 
 );
