a392e86f240c9c354ca0417fd426b3548f07bef6
Rahul <velurediff@gmail.com>
2026-07-28 11:12:29 +0530

    Add ProfileView.vue for student profile display

---
+++ Module2_FrontendDev/Velu Rahul L/handson_08/student-portal-vue/src/views/ProfileView.vue
@@ -0,0 +1,45 @@
+<template>
+
+<div class="container">
+
+<h1>
+
+Student Profile
+
+</h1>
+
+<p>
+
+Name : Rahul
+
+</p>
+
+<p>
+
+Department : Computer Science
+
+</p>
+
+<p>
+
+Semester : 6
+
+</p>
+
+</div>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+.container{
+
+padding:30px;
+
+}
+
+</style>
