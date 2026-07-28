6aa9ac63fd2f28a8ca9c3b0cb2b69e383659e219
Rahul <velurediff@gmail.com>
2026-07-28 10:17:16 +0530

    Add Footer component with styling and content

---
+++ Module2_FrontendDev/Velu Rahul L/handson_05/student-portal-react/src/components/Footer.jsx
@@ -0,0 +1,23 @@
+function Footer() {
+
+    return (
+
+        <footer
+            style={{
+                background: "#1565c0",
+                color: "white",
+                textAlign: "center",
+                padding: "20px",
+                marginTop: "30px"
+            }}
+        >
+
+            © 2026 Student Portal | Digital Nurture 5.0
+
+        </footer>
+
+    );
+
+}
+
+export default Footer;
