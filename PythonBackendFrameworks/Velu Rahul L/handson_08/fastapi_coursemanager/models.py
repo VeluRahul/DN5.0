0d4dcde10b17094a2fbda6affc8716395ce476ff
Rahul <velurediff@gmail.com>
2026-07-28 12:58:21 +0530

    Create models.py

---
+++ PythonBackendFrameworks/Velu Rahul L/handson_08/fastapi_coursemanager/models.py
@@ -0,0 +1,35 @@
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+from sqlalchemy import Integer
+from sqlalchemy import String
+
+
+class Base(DeclarativeBase):
+
+    pass
+
+
+class Course(Base):
+
+    __tablename__ = "courses"
+
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+        index=True
+    )
+
+    name: Mapped[str] = mapped_column(
+        String(100)
+    )
+
+    code: Mapped[str] = mapped_column(
+        String(20),
+        unique=True
+    )
+
+    credits: Mapped[int]
+
+    department_id: Mapped[int]
