3f93c55855ce24efde58f839784ee119035be2e2
Rahul <velurediff@gmail.com>
2026-07-28 11:29:20 +0530

    Add course selectors for courses, loading, and error

---
+++ Module2_FrontendDev/Velu Rahul L/handson_10/student-portal-react/src/selectors/courseSelectors.js
@@ -0,0 +1,17 @@
+export const selectCourses =
+
+(state)=>
+
+state.courses.courses;
+
+export const selectCoursesLoading=
+
+(state)=>
+
+state.courses.loading;
+
+export const selectCoursesError=
+
+(state)=>
+
+state.courses.error;
