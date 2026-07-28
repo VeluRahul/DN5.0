0afabe42e2aba3bd975548c23507b7d2c0d81a29
Rahul <velurediff@gmail.com>
2026-07-28 14:45:12 +0530

    Update conftest.py

--- SeleniumBasics/Velu Rahul L/handson_07/conftest.py
+++ SeleniumBasics/Velu Rahul L/handson_07/conftest.py
@@ -1,1 +1,27 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
+
+@pytest.fixture(scope="session")
+def base_url():
+    return "https://www.lambdatest.com/selenium-playground/"
+
+
+@pytest.fixture(scope="function")
+def driver():
+
+    options = Options()
+
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=options
+    )
+
+    driver.maximize_window()
+
+    yield driver
+
+    driver.quit()
