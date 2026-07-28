df3a3685949b97044c0fef3620302747a1d59aa0
Rahul <velurediff@gmail.com>
2026-07-28 10:42:39 +0530

    Create ProfilePage.jsx

---
+++ Module2_FrontendDev/Velu Rahul L/handson_06/student-portal-react/src/pages/ProfilePage.jsx
@@ -0,0 +1,37 @@
+function ProfilePage(){
+
+return(
+
+<div className="container">
+
+<h1>
+
+Student Profile
+
+</h1>
+
+<p>
+
+Name : Rahul
+
+</p>
+
+<p>
+
+Department : Computer Science
+
+</p>
+
+<p>
+
+Semester : 6
+
+</p>
+
+</div>
+
+);
+
+}
+
+export default ProfilePage;
