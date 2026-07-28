5dbe5f5352cd2fc4c09a92f919e87b9260d463b1
Rahul <velurediff@gmail.com>
2026-07-28 10:41:46 +0530

    Add HomePage component for student portal

---
+++ Module2_FrontendDev/Velu Rahul L/handson_06/student-portal-react/src/pages/HomePage.jsx
@@ -0,0 +1,27 @@
+function HomePage(){
+
+return(
+
+<div className="container">
+
+<h1>
+
+Welcome to Student Portal
+
+</h1>
+
+<p>
+
+Digital Nurture 5.0
+
+React Routing Demo
+
+</p>
+
+</div>
+
+);
+
+}
+
+export default HomePage;
