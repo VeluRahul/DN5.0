de36bb45667394a1efe8b9a7c31af223b2a2693f
Rahul <velurediff@gmail.com>
2026-07-28 12:21:33 +0530

    Create asgi.py

---
+++ PythonBackendFrameworks/Velu Rahul L/handson_02/coursemanager/coursemanager/asgi.py
@@ -0,0 +1,14 @@
+"""
+ASGI config for asynchronous deployment.
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'coursemanager.settings'
+)
+
+application = get_asgi_application()
