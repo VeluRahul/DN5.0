9e052136a290de49bf6c9111f87885270db6968e
Rahul <velurediff@gmail.com>
2026-07-28 12:27:48 +0530

    Create serializers.py

---
+++ PythonBackendFrameworks/Velu Rahul L/handson_03/coursemanager/coursemanager/serializers.py
@@ -0,0 +1,42 @@
+from rest_framework import serializers
+
+from .models import Department
+from .models import Course
+from .models import Student
+from .models import Enrollment
+
+
+class DepartmentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = Department
+
+        fields = "__all__"
+
+
+class CourseSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = Course
+
+        fields = "__all__"
+
+
+class StudentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = Student
+
+        fields = "__all__"
+
+
+class EnrollmentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = Enrollment
+
+        fields = "__all__"
