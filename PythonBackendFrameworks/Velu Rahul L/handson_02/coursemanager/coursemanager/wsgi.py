91b682981cac731ed6e6ab722c06a28e467ebcff
Rahul <velurediff@gmail.com>
2026-07-28 12:21:06 +0530

    Create wsgi.py

---
+++ PythonBackendFrameworks/Velu Rahul L/handson_02/coursemanager/coursemanager/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for synchronous deployment.
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'coursemanager.settings'
+)
+
+application = get_wsgi_application()
