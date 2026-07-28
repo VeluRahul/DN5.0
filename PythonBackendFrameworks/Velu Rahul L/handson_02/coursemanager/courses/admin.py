b72340af292ae786ad8bdfbc60ac774797ff11c8
Rahul <velurediff@gmail.com>
2026-07-28 12:16:01 +0530

    Create admin.py

---
+++ PythonBackendFrameworks/Velu Rahul L/handson_02/coursemanager/courses/admin.py
@@ -0,0 +1,86 @@
+from django.contrib import admin
+
+from .models import Department
+from .models import Course
+from .models import Student
+from .models import Enrollment
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+
+    list_display = [
+        "name",
+        "head_of_dept",
+        "budget"
+    ]
+
+    search_fields = [
+        "name",
+        "head_of_dept"
+    ]
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+
+    list_display = [
+        "name",
+        "code",
+        "credits",
+        "department"
+    ]
+
+    search_fields = [
+        "name",
+        "code"
+    ]
+
+    list_filter = [
+        "department"
+    ]
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+
+    list_display = [
+        "first_name",
+        "last_name",
+        "email",
+        "department",
+        "enrollment_year"
+    ]
+
+    search_fields = [
+        "first_name",
+        "last_name",
+        "email"
+    ]
+
+    list_filter = [
+        "department",
+        "enrollment_year"
+    ]
+
+
+@admin.register(Enrollment)
+class EnrollmentAdmin(admin.ModelAdmin):
+
+    list_display = [
+        "student",
+        "course",
+        "enrollment_date",
+        "grade"
+    ]
+
+    list_filter = [
+        "course",
+        "grade"
+    ]
+
+    search_fields = [
+        "student__first_name",
+        "student__last_name",
+        "course__name"
+    ]
