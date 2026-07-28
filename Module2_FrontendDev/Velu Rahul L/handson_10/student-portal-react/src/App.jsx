335ea5d42b7efe49318980cc520857a7b8c2eb18
Rahul <velurediff@gmail.com>
2026-07-28 11:31:47 +0530

    Create App.jsx

---
+++ Module2_FrontendDev/Velu Rahul L/handson_10/student-portal-react/src/App.jsx
@@ -0,0 +1,28 @@
+import CoursesPage from "./pages/CoursesPage";
+
+function App(){
+
+return(
+
+<div>
+
+<h1
+style={{
+textAlign:"center",
+margin:"20px"
+}}
+>
+
+Student Portal
+
+</h1>
+
+<CoursesPage/>
+
+</div>
+
+);
+
+}
+
+export default App;
